512973e0eba818f9d096d33a69753a628eb977f7
korih <96402323+korih@users.noreply.github.com>
2025-01-26 16:47:05 -0800

    single page template basics added

---
+++ single-page-landing/src/App.tsx
@@ -0,0 +1,15 @@
+import Feature from "./components/Feature"
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+
+function App() {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Feature />
+    </main>
+  )
+}
+
+export default App
